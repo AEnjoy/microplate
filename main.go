003c3f2b15178f56df4cd5bc152ae33b63cc9e8e
AEnjoy <2328790874@qq.com>
2024-05-23 16:40:40 +0800

    v1

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
+const url = "http://deviceshifu-plate-reader.deviceshifu.svc.cluster.local/get_measurement"
+
+func main() {
+	client := &http.Client{}
+
+	resp, _ := client.Get(url)
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	str := strings.Split(string(body), "\n")
+	count := 0
+	value := 0.0
+	for _, s := range str {
+		numStr := strings.Split(s, " ")
+		for _, s2 := range numStr {
+			float, err := strconv.ParseFloat(s2, 64)
+			if err != nil {
+				return
+			}
+			count++
+			value += float
+		}
+	}
+	if count != 0 {
+		fmt.Printf("%.2f", value/float64(count))
+	}
 }
