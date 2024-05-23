1764609ca778b697821c15a27dab773637b93f39
AEnjoy <2328790874@qq.com>
2024-05-23 22:09:11 +0800

    code completed

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,9 @@
-FROM golang:1.22-alpine AS builder
-# https://github.com/chaseSpace/k8s-tutorial-cn
-# 缓存依赖
-WORKDIR /go/cache
-COPY go.mod .
-# COPY go.sum .
-RUN GOPROXY=https://goproxy.cn,direct go mod tidy
+FROM golang:latest
 
-WORKDIR /build
-COPY . .
+COPY . /microplate
 
-# 关闭cgo的原因：使用了多阶段构建，go程序的编译环境和运行环境不同，不关就无法运行go程序
-RUN GOOS=linux CGO_ENABLED=0 GOARCH=amd64 GO111MODULE=auto go build -ldflags "-w -extldflags -static" -o main
+WORKDIR /microplate
 
-#FROM scratch as prod
-FROM alpine as prod
-# 通过 http://www.asznl.com/post/48 了解docker基础镜像：scratc、busybox、alpine
-# 比他们还小的是distroless   由谷歌提供，了解：https://github.iotroom.top/GoogleContainerTools/distroless
+RUN GOOS=linux CGO_ENABLED=0 GOARCH=amd64 GO111MODULE=auto go build -ldflags "-w -extldflags -static" -o main main.go
 
-# alpine设置时区
-RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories &&  \
-    apk add -U tzdata && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && apk del tzdata && date
-
-COPY --from=builder /build/main .
-
-EXPOSE 3000
-ENTRYPOINT ["/main"]
+CMD ["./main"]
--- main.go
+++ main.go
@@ -2,44 +2,42 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const url = "http://deviceshifu-plate-reader.deviceshifu.svc.cluster.local/get_measurement"
 
 func main() {
 	client := &http.Client{}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "[v4] Running env test")
-	})
-	resp, err := client.Get(url)
-	if err != nil {
-		fmt.Errorf("Error: %s", err)
-	} else {
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		str := strings.Split(string(body), "\n")
-		count := 0
-		value := 0.0
-		for _, s := range str {
-			numStr := strings.Split(s, " ")
-			for _, s2 := range numStr {
-				float, err := strconv.ParseFloat(s2, 64)
-				if err != nil {
-					return
+	ticker := time.NewTicker(3 * time.Second)
+	for range ticker.C {
+		resp, err := client.Get(url)
+		if err != nil {
+			fmt.Errorf("error: %s\n", err)
+		} else {
+			body, _ := ioutil.ReadAll(resp.Body)
+			str := strings.Split(string(body), "\n")
+			count := 0
+			value := 0.0
+			for _, s := range str {
+				numStr := strings.Split(s, " ")
+				for _, s2 := range numStr {
+					float, err := strconv.ParseFloat(s2, 64)
+					if err != nil {
+						continue
+					}
+					count++
+					value += float
 				}
-				count++
-				value += float
 			}
-		}
-		if count != 0 {
-			fmt.Printf("%.2f", value/float64(count))
+			if count != 0 {
+				fmt.Printf("%.2f\n", value/float64(count))
+			}
+			resp.Body.Close()
 		}
 	}
-
-	http.ListenAndServe(":3000", nil)
 }
